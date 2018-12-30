369f371ab5e5b201f5393e58e4c5c3e35dd629b8
Rishitha Minol <rishithaminol@gmail.com>
2018-12-30 17:32:31 +0530

    Removed the flask and using python-telegram-bot features behind the nginx

--- server.py
+++ server.py
@@ -1,54 +1,23 @@
 #!/usr/bin/env python
 
-"""
-HTTP server example taken from - https://blog.anvileight.com/posts/simple-python-http-server/
-"""
-
 import sys
 import os
 sys.path.append('./src')
 
-import telegram
+from telegram import Bot
+from telegram.ext import MessageHandler, Dispatcher, Updater, CommandHandler
 import config	# Local package
 import json
 from colored import fg, bg, attr
 
-from flask import Flask, request, make_response, jsonify
+updater = Updater(token=config.get_token())
+dispatcher = updater.dispatcher
 
-bot = telegram.Bot(token=config.get_token())
-app = Flask(__name__)
+def start_func(bot, update):
+	bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
-# Configuration
-app.config['DEBUG'] = config.DEBUG
-if config.DEBUG:
-	app.config['ENV'] = 'development'
+start_handler = CommandHandler('start', start_func)
+dispatcher.add_handler(start_handler)
 
-@app.route('/', methods=['GET', 'POST'])
-def main_handler():
-	print(fg(45) + str(request.headers.get('Content-Type')) + attr(0))
-	body_str = json.dumps(request.get_json(), indent=4, sort_keys=True)
-	print(fg(214) + body_str + attr(0))
-
-	if request.is_json:
-		print(fg(220) + "Request contains valid data" + attr(0))
-
-	if request.method == 'GET':
-		resp_body = jsonify({'application': 'Telgram bot'})
-	else:
-		resp_body = jsonify(bot_handler(request.get_json()))
-
-	resp = make_response(resp_body, 200)
-	resp.headers['Content-Type'] = 'application/json'
-	return resp
-
-def bot_handler(body_dict):
-	# Generate update object
-	update = telegram.Update.de_json(body_dict, bot)
-
-	# Check for authorized user valid user
-	print(update.message.from_user.id)
-
-	return {"ok": 200}
-
-if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+updater.start_webhook(listen='0.0.0.0', port=5000)
+print("hello")
